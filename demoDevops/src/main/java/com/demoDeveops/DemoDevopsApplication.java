063f9384b9aeefe40f792e2a402a9e3eb1100c54
cstar1994 <04pankajb@gmail.com>
2018-09-10 15:01:47 +0530

    Initial commit for the demo project

---
+++ demoDevops/src/main/java/com/demoDeveops/DemoDevopsApplication.java
@@ -0,0 +1,12 @@
+package com.demoDeveops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevopsApplication.class, args);
+	}
+}
